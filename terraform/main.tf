fe914281156e9d59ac4736aa52f767ab1e0d5e8a
Arzu Qafarov <156705532+gaffarov-arzu@users.noreply.github.com>
2025-12-16 18:53:29 +0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"  # AWS test AMI
+  instance_type = "t2.micro"
+}
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "my-portfolio-bucket-12345"  # benzersiz bir isim
+  acl    = "private"
+}
