2d2379e4a00e96300b0d404f7b885ca2d69525b6
Arzu Qafarov <156705532+gaffarov-arzu@users.noreply.github.com>
2025-12-21 21:57:16 +0400

    Update main.tf

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -5,18 +5,18 @@ resource "aws_security_group" "app_sg" {
 
   ingress {
     from_port   = 22
-OBOBOB    to_port     = 22
+    to_port     = 22
     protocol    = "tcp"
-OBOBOB    cidr_blocks = ["0.0.0.0/0"]
-  }
-OBOBOBOBOBOB
-  ingress {
-    from_port   = 80
-OBOBOB    to_port     = 80
-OBOBOB    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-OBOBOB
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
