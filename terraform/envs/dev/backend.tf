4879dfb53d1f921c9f9556c34c5dbbcc3e560bcb
Arzu Qafarov <156705532+gaffarov-arzu@users.noreply.github.com>
2025-12-21 22:31:45 +0400

    Update backend.tf

--- terraform/envs/dev/backend.tf
+++ terraform/envs/dev/backend.tf
@@ -1,7 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket = "my-terraform-state-bucket"
-    key    = "infra-automation/dev/terraform.tfstate"
-    region = "us-west-2"  # burayı bucket’ın gerçek bölgesi ile değiştir
-  }
-}
+
